caf839e61bff659734b459cfc046fa5bfe82df59
Miloš Roknić <roknic.milos.994@gmail.com>
2024-10-13 15:01:05 +0200

    Add icons

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ set -x # Enable printing of each command
 mkdir -p /app/build
 cp /app/src/index.html /app/build/index.html
 cp /app/src/style.css /app/build/style.css
+cp -r /app/src/assets /app/build/assets
 
 ls -la /app/src
 cat /app/src/.env
