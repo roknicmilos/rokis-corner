5d54920ab4d3dd548fa630a32effc9e228335061
Miloš Roknić <roknic.milos.994@gmail.com>
2024-10-13 13:21:50 +0200

    Remove console logs from src/build.js

--- src/build.js
+++ src/build.js
@@ -4,7 +4,6 @@ const dotenv = require("dotenv");
 
 dotenv.config({ path: path.resolve(__dirname, ".env") });
 const htmlFilePath = path.join(__dirname, "..", "build", "index.html");
-console.log("process.env:", process.env); // TODO: REMOVE
 let html = fs.readFileSync(htmlFilePath, "utf-8");
 html = html.replace(/@portfolioURL/g, process.env.PORTFOLIO_URL);
 fs.writeFileSync(htmlFilePath, html, "utf-8");
