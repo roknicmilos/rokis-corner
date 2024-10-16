d4a13f9ca0eb198e6716bed15b50eb6d64cb4d8b
Miloš Roknić <roknic.milos.994@gmail.com>
2024-10-16 19:10:45 +0200

    Add Google Analytics tracker code

--- src/build.js
+++ src/build.js
@@ -3,7 +3,19 @@ const path = require("path");
 const dotenv = require("dotenv");
 
 dotenv.config({ path: path.resolve(__dirname, ".env") });
+
 const htmlFilePath = path.join(__dirname, "..", "build", "index.html");
 let html = fs.readFileSync(htmlFilePath, "utf-8");
+
 html = html.replace(/@portfolioURL/g, process.env.PORTFOLIO_URL);
+
+if (process.env.GOOGLE_ANALYTICS_TRACKING_ID) {
+    const gaTemplatePath = path.join(__dirname, "google_analytics.html");
+    let googleAnalyticsTemplate = fs.readFileSync(gaTemplatePath, "utf-8");
+    googleAnalyticsTemplate = googleAnalyticsTemplate.replace(
+        /@googleAnalyticsTrackingID/g, process.env.GOOGLE_ANALYTICS_TRACKING_ID
+    );
+    html = html.replace("</body>", `${googleAnalyticsTemplate}\n</body>`);
+}
+
 fs.writeFileSync(htmlFilePath, html, "utf-8");
