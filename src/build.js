725c49166fa2346cb684992f2a767329e05cf1bb
Miloš Roknić <roknic.milos.994@gmail.com>
2024-12-28 14:14:54 +0100

    Change the page text and image

--- src/build.js
+++ src/build.js
@@ -11,13 +11,4 @@ html = html.replace(/@myPortfolioURL/g, process.env.MY_PORTFOLIO_URL);
 
 html = html.replace(/@portfolizerURL/g, process.env.PORTFOLIZER_URL);
 
-if (process.env.GOOGLE_ANALYTICS_TRACKING_ID) {
-    const gaTemplatePath = path.join(__dirname, "google_analytics.html");
-    let googleAnalyticsTemplate = fs.readFileSync(gaTemplatePath, "utf-8");
-    googleAnalyticsTemplate = googleAnalyticsTemplate.replace(
-        /@googleAnalyticsTrackingID/g, process.env.GOOGLE_ANALYTICS_TRACKING_ID
-    );
-    html = html.replace("</body>", `${googleAnalyticsTemplate}\n</body>`);
-}
-
 fs.writeFileSync(htmlFilePath, html, "utf-8");
